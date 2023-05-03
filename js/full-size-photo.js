63a920a0893120b23927947e689c671d500a89ee
Bunakov Dmitry <floppy-a@yandex.ru>
2023-05-03 17:39:00 +0500

    feat: add close popup full size photo out click; indent comment

--- js/full-size-photo.js
+++ js/full-size-photo.js
@@ -2,6 +2,9 @@ const fullSizeView = document.querySelector('.big-picture');
 const fullSizePhoto = fullSizeView.querySelector('.big-picture__img');
 const fullSizePhotoSocial = fullSizeView.querySelector('.big-picture__social');
 const buttonCommentsLoader = fullSizeView.querySelector('.comments-loader');
+const containerForComments = document.querySelector('.social__comments');
+const similarCommentTemplate = containerForComments.querySelector('.social__comment');
+const containerForCommentsFragment = document.createDocumentFragment();
 
 const isCommentOff = (comments) => {
   if (fullSizeView.getElementsByClassName('social__comment')[comments.length-1].className.split(' ').indexOf('hidden') >= 0) {
@@ -21,24 +24,23 @@ const buttonCommentsLoaderClickHandler = () => {
   if (fullSizeView.querySelector('.social__comments .hidden')) {
     buttonCommentsLoader.classList.remove('hidden');
   }
-  fullSizePhotoSocial.querySelector('.social__comment-count').innerHTML = `${Array.from(document.querySelectorAll('.social__comment')).length - Array.from(document.querySelectorAll('.social__comments .hidden')).length} из <span className="comments-count">${Array.from(document.querySelectorAll('.social__comment')).length}</span> комментариев`;
+  fullSizePhotoSocial.querySelector('.social__comment-count').innerHTML = `${Array.from(document.querySelectorAll('.social__comment')).length - Array.from(document.querySelectorAll('.social__comments .hidden')).length} из <span class="comments-count">${Array.from(document.querySelectorAll('.social__comment')).length}</span> комментариев`;
 };
 const renderFullSizePhoto = (url, description, likes, comments) => {
   fullSizeView.classList.remove('hidden');
   fullSizePhoto.querySelector('img').src = url;
   fullSizePhotoSocial.querySelector('.likes-count').textContent = likes;
-  fullSizePhotoSocial.querySelector('.social__comment-count').innerHTML = `${Math.min(5, comments.length)} из <span className="comments-count">${comments.length}</span> комментариев`;
+  fullSizePhotoSocial.querySelector('.social__comment-count').innerHTML = `${Math.min(5, comments.length)} из <span class="comments-count">${comments.length}</span> комментариев`;
   fullSizePhotoSocial.querySelector('.social__caption').textContent = description;
   const commentsList = fullSizeView.querySelector('.social__comments');
   commentsList.textContent = '';
   comments.forEach((comment, index) => {
     const commentElement = document.createElement('li');
     commentElement.classList.add('social__comment');
-    commentElement.innerHTML = `<img className="social__picture" src="${comment.avatar}" alt="${comment.name}" width="35" height="35"> <p className="social__text">${comment.message}</p>`;
+    commentElement.innerHTML = `<img class="social__picture" src="${comment.avatar}" alt="${comment.name}" width="35" height="35"> <p class="social__text">${comment.message}</p>`;
     if (index >= 5) {
       commentElement.classList.add('hidden');
     }
-    commentElement.setAttribute('data-index', index);
     commentsList.appendChild(commentElement);
   });
   buttonCommentsLoader.addEventListener('click', buttonCommentsLoaderClickHandler);
@@ -46,6 +48,7 @@ const renderFullSizePhoto = (url, description, likes, comments) => {
   fullSizeView.addEventListener('click', (evt) => {
     if (evt.target.classList.contains('big-picture')) {
       fullSizeView.classList.add('hidden');
+      document.body.classList.remove('modal-open');
     }
   });
   return fullSizeView;
--- js/thumbnail.js
+++ js/thumbnail.js
@@ -3,20 +3,20 @@ const containerForPictures = document.querySelector('.pictures');
 const similarPhotoTemplate = document.querySelector('#picture').content.querySelector('a');
 const containerFragment = document.createDocumentFragment();
 
-const similarTopicsCallback = (topic) => {
+const createSimilarClonedTopics = (topic) => {
+  const {url, likes, comments, description} = topic;
   const topicElement = similarPhotoTemplate.cloneNode(true);
-  topicElement.querySelector('img').src = topic.url;
-  topicElement.querySelector('.picture__likes').textContent = topic.likes;
-  topicElement.querySelector('.picture__comments').textContent = topic.comments.length;
+  topicElement.querySelector('img').src = url;
+  topicElement.querySelector('.picture__likes').textContent = likes;
+  topicElement.querySelector('.picture__comments').textContent = comments.length;
   topicElement.addEventListener('click', () => {
-    renderFullSizePhoto(topic.url, topic.description, topic.likes, topic.comments);
+    renderFullSizePhoto(url, description, likes, comments);
     document.body.classList.add('modal-open');
   });
   containerFragment.appendChild(topicElement);
 };
-
 const renderThumbnails = (similarTopics) => {
-  similarTopics.forEach((topic) => {similarTopicsCallback(topic);});
+  similarTopics.forEach((topic) => {createSimilarClonedTopics(topic);});
   containerForPictures.appendChild(containerFragment);
 };
 
